66a34bc99e1ca6fc75ccfaef769af5c37194a277
Saad <mohd.saadhaider@gmail.com>
2025-11-27 22:49:53 +0530

    add: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/next"
+
 import Footer from "@/components/Footer";
 import LocomotiveScrollWrapper from "@/components/LocomotiveScroll";
 
@@ -38,6 +40,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </LocomotiveScrollWrapper>
+        <Analytics />
       </body>
     </html>
   );
